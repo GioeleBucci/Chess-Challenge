b65da32f15edac93c3e51c92f5891591e6f9beb7
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2023-07-25 16:11:34 +0200

    feat: added checks for draws and checkmates, now MyBot almost always beats EvilBot. For some reason alpha-beta pruning doesn't work though.

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,20 +6,13 @@ using System.Linq;
 public class MyBot : IChessBot
 {
     private static readonly int MAX_DEPTH = 3;
-
-
-    private bool earlyGame = true, midGame = false, endGame = false;
-    /* An integer that memorises the number of times this "Think" function was called;
-     useful to determine the current state of the match. */
-    private int callNumber = 0;
     
     private static Random random = new();
     
-    private Move? lastMove = null;
     private Move moveToDo;
     private Move[] allMoves;
 
-    private Stack<Move> movesDoneByMiniMax = new();
+    //private Stack<Move> movesDoneByMiniMax = new(); //debug only
 
     private bool botIsWhite;
     
@@ -30,42 +23,14 @@ public class MyBot : IChessBot
     {
         allMoves = board.GetLegalMoves();
         botIsWhite = board.IsWhiteToMove;
-
-        if (earlyGame)
-            EarlyPolicy(board);
-        else
-            MidGamePolicy(board);
-        CheckIfMoveIsStillLegal(board);
+        MidGamePolicy(board);
         return moveToDo;
     }
 
     private void RandomMove() => moveToDo = allMoves[random.Next(allMoves.Length)];
 
-    private void EarlyPolicy(Board board) 
-    {
-        if (++this.callNumber >= 5)
-        { 
-            earlyGame = false;
-            midGame = true;
-        }
-        if (!this.lastMove.HasValue)
-            RandomMove();
-
-        // Avoid moving the same piece during openings
-        else
-            do
-            {
-                RandomMove();
-                //Console.WriteLine("Move to do: " + moveToDo.ToString()); // debug only
-                //Console.WriteLine("Last move: " + lastMove.ToString()); // debug only
-            } while (lastMove.Value.TargetSquare.Name.Equals(moveToDo.StartSquare.Name) && allMoves.Length > 1);
-            // if there's only one move available it should be played even if it implies moving the same piece
-
-        lastMove = moveToDo;
-    }
-
     private void MidGamePolicy(Board board) {
-        MiniMaxAlfaBetaPruning(board, MAX_DEPTH, int.MinValue, int.MaxValue, true);
+        MiniMaxAlfaBetaPruning(board, MAX_DEPTH, MAX_DEPTH, int.MinValue, int.MaxValue, true);
         //Console.WriteLine("****************************************************************\n\n");
     }
 
@@ -80,9 +45,9 @@ public class MyBot : IChessBot
         return true;
     }
 
-    private int MiniMaxAlfaBetaPruning(Board board, int depth, int alpha, int beta, bool maximizingPlayer) 
+    private int MiniMaxAlfaBetaPruning(Board board, int maxDepth, int currentDepth, int alpha, int beta, bool maximizingPlayer) 
     {
-        if (depth == 0 || board.IsInCheckmate() || board.IsDraw())
+        if (currentDepth == 0 || board.IsInCheckmate() || board.IsDraw())
             return Evaluate(board);
 
         Move[] allPossibleMoves = board.GetLegalMoves();
@@ -93,23 +58,26 @@ public class MyBot : IChessBot
             foreach (Move move in allPossibleMoves)
             {
                 board.MakeMove(move);
-                movesDoneByMiniMax.Push(move);
+                //movesDoneByMiniMax.Push(move);
                 //PrettyPrintDebug(depth, MAX_DEPTH);
                 //Console.WriteLine("Pushed move " + move.ToString() + "at depth " + depth);
-                int evaluation = MiniMaxAlfaBetaPruning(board, depth - 1, alpha, beta, false);
+                int evaluation = MiniMaxAlfaBetaPruning(board, maxDepth, currentDepth - 1, alpha, beta, false);
                 board.UndoMove(move);
-                var popped = movesDoneByMiniMax.Pop();
+                //var popped = movesDoneByMiniMax.Pop();
                 //PrettyPrintDebug(depth, MAX_DEPTH);
                 //Console.WriteLine("Popped move: " + popped.ToString(), ", expected: " + move.ToString());
+                /*
                 if (!popped.ToString().Equals(move.ToString()))
                 {
                     Console.WriteLine("INCONSISTENCY FOUND!\n\n");
                     System.Environment.Exit(-12);
                 }
+                */
+                alpha = Math.Max(alpha, evaluation);
                 if (evaluation >= maxEvaluation)
                 {
                     maxEvaluation = evaluation;
-                    if (depth == MAX_DEPTH)
+                    if (currentDepth == maxDepth)
                         moveToDo = move;
                     //Console.WriteLine("Depth is: " + depth + ", in the MiniMax function the move is: " + moveToDo.ToString() +
                       //" and the move is " + (CheckIfMoveIsStillLegal(board) ? "legal" : "ILLEGAL"));
@@ -121,21 +89,22 @@ public class MyBot : IChessBot
             foreach (Move move in allPossibleMoves)
             {
                 board.MakeMove(move);
-                movesDoneByMiniMax.Push(move);
+                //movesDoneByMiniMax.Push(move);
                 //PrettyPrintDebug(depth, MAX_DEPTH);
                 //Console.WriteLine("Pushed move " + move.ToString() + "at depth " + depth);
-                int evaluation = MiniMaxAlfaBetaPruning(board, depth - 1, alpha, beta, true);
+                int evaluation = MiniMaxAlfaBetaPruning(board, maxDepth, currentDepth - 1, alpha, beta, true);
                 board.UndoMove(move);
-                var popped = movesDoneByMiniMax.Pop();
+                //var popped = movesDoneByMiniMax.Pop();
                 //PrettyPrintDebug(depth, MAX_DEPTH);
                 //Console.WriteLine("Popped move: " + popped.ToString(), ", expected: " + move.ToString());
+                /*
                 if (!popped.ToString().Equals(move.ToString()))
                 {
                     Console.WriteLine("INCONSISTENCY FOUND!\n\n");
                     System.Environment.Exit(-12);
                 }
+                */
                 minEvaluation = Math.Min(minEvaluation, evaluation);
-                beta = Math.Min(beta, evaluation);
             }
             return minEvaluation;
         }
@@ -143,14 +112,21 @@ public class MyBot : IChessBot
 
     private int Evaluate(Board board) 
     {
+        int score = 0;
+        if (board.IsDraw())
+            return score;
+        if (board.IsInCheckmate())
+            return int.MaxValue;
         int whiteMaterial = 0, blackMaterial = 0, i;
         for (i = 0; i < 6; i++)
             whiteMaterial += board.GetAllPieceLists()[i].Count * pieceValues[i + 1];
         for (i = 0; i < 6; i++)
             blackMaterial += board.GetAllPieceLists()[i + 6].Count * pieceValues[i + 1];
         if (botIsWhite)
-            return whiteMaterial - blackMaterial;
-        return - (whiteMaterial - blackMaterial);
+            score = whiteMaterial - blackMaterial;
+        else 
+            score = blackMaterial - whiteMaterial;
+        return score;
     }
 
     private void PrettyPrintDebug(int currentDepth, int maxDepth)
