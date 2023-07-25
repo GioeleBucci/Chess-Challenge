d6bb6ada026bba33497bf92ffde94e7f85177290
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2023-07-25 15:11:39 +0200

    debug: pushed printing system to see moves in a stack

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,5 +1,7 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
@@ -13,6 +15,10 @@ public class MyBot : IChessBot
     private Move? lastMove = null;
     private Move moveToDo;
     private Move[] allMoves;
+
+    private Stack<Move> movesDoneByMiniMax = new();
+
+    private bool botIsWhite;
     
     // Piece values: null, pawn, knight, bishop, rook, queen, king
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
@@ -20,11 +26,13 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         allMoves = board.GetLegalMoves();
+        botIsWhite = board.IsWhiteToMove;
 
         if (earlyGame)
             EarlyPolicy(board);
         else
             MidGamePolicy(board);
+        CheckIfMoveIsStillLegal(board);
         return moveToDo;
     }
 
@@ -47,23 +55,108 @@ public class MyBot : IChessBot
                 RandomMove();
                 //Console.WriteLine("Move to do: " + moveToDo.ToString()); // debug only
                 //Console.WriteLine("Last move: " + lastMove.ToString()); // debug only
-            } while (lastMove.Value.TargetSquare.Name.Equals(moveToDo.StartSquare.Name));
+            } while (lastMove.Value.TargetSquare.Name.Equals(moveToDo.StartSquare.Name) && allMoves.Length > 1);
+            // if there's only one move available it should be played even if it implies moving the same piece
 
         lastMove = moveToDo;
     }
 
     private void MidGamePolicy(Board board) {
-        int highestValueCapture = 0;
-        foreach (var move in allMoves) 
-        {
-            Piece capturedPiece = board.GetPiece(move.TargetSquare);
-            int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+        MiniMaxAlfaBetaPruning(board, 3, int.MinValue, int.MaxValue, true);
+        //Console.WriteLine("****************************************************************\n\n");
+    }
 
-            if (capturedPieceValue >= highestValueCapture)
+    private bool CheckIfMoveIsStillLegal(Board board)
+    {
+        if (!board.GetLegalMoves().Contains(moveToDo))
+        {
+            //Console.WriteLine("Illegal move detected: " + moveToDo.ToString());
+            //RandomMove(); //without this I still get illegal moves...
+            return false;
+        }
+        return true;
+    }
+
+    private int MiniMaxAlfaBetaPruning(Board board, int depth, int alpha, int beta, bool maximizingPlayer) 
+    {
+        if (depth == 0 || board.IsInCheckmate() || board.IsDraw())
+            return Evaluate(board);
+
+        Move[] allPossibleMoves = board.GetLegalMoves();
+
+        if (maximizingPlayer) 
+        {
+            int maxEvaluation = int.MinValue;
+            foreach (Move move in allPossibleMoves)
             {
-                moveToDo = move;
-                highestValueCapture = capturedPieceValue;
+                board.MakeMove(move);
+                movesDoneByMiniMax.Push(move);
+                PrettyPrintDebug(depth, 3);
+                Console.WriteLine("Pushed move " + move.ToString() + "at depth " + depth);
+                int evaluation = MiniMaxAlfaBetaPruning(board, depth - 1, alpha, beta, false);
+                board.UndoMove(move);
+                var popped = movesDoneByMiniMax.Pop();
+                PrettyPrintDebug(depth, 3);
+                Console.WriteLine("Popped move: " + popped.ToString(), ", expected: " + move.ToString());
+                if (!popped.ToString().Equals(move.ToString()))
+                {
+                    Console.WriteLine("INCONSISTENCY FOUND!\n\n");
+                    System.Environment.Exit(-12);
+                }
+                if (evaluation >= maxEvaluation)
+                {
+                    maxEvaluation = evaluation;
+                    moveToDo = move;
+                    //Console.WriteLine("Depth is: " + depth + ", in the MiniMax function the move is: " + moveToDo.ToString() +
+                      //" and the move is " + (CheckIfMoveIsStillLegal(board) ? "legal" : "ILLEGAL"));
+                }
             }
+            return maxEvaluation;
+        } else {
+            int minEvaluation = int.MaxValue;
+            foreach (Move move in allPossibleMoves)
+            {
+                board.MakeMove(move);
+                movesDoneByMiniMax.Push(move);
+                PrettyPrintDebug(depth, 3);
+                Console.WriteLine("Pushed move " + move.ToString() + "at depth " + depth);
+                int evaluation = MiniMaxAlfaBetaPruning(board, depth - 1, alpha, beta, true);
+                board.UndoMove(move);
+                var popped = movesDoneByMiniMax.Pop();
+                PrettyPrintDebug(depth, 3);
+                Console.WriteLine("Popped move: " + popped.ToString(), ", expected: " + move.ToString());
+                if (!popped.ToString().Equals(move.ToString()))
+                {
+                    Console.WriteLine("INCONSISTENCY FOUND!\n\n");
+                    System.Environment.Exit(-12);
+                }
+                minEvaluation = Math.Min(minEvaluation, evaluation);
+                beta = Math.Min(beta, evaluation);
+            }
+            return minEvaluation;
+        }
+    }
+
+    private int Evaluate(Board board) 
+    {
+        int whiteMaterial = 0, blackMaterial = 0, i;
+        for (i = 0; i < 6; i++)
+            whiteMaterial += board.GetAllPieceLists()[i].Count * pieceValues[i + 1];
+        for (i = 0; i < 6; i++)
+            blackMaterial += board.GetAllPieceLists()[i + 6].Count * pieceValues[i + 1];
+        if (botIsWhite)
+            return whiteMaterial - blackMaterial;
+        return - (whiteMaterial - blackMaterial);
+    }
+
+    private void PrettyPrintDebug(int currentDepth, int maxDepth)
+    {
+        /* This method is used to print some tabs before writing a line containing the moves pushed
+         and popped by the caller. It visually represents the depths at which the moves are done.*/
+        for (int j = 0; j < maxDepth; j++)
+        {
+            for (int xx = Math.Abs(maxDepth - currentDepth); xx > 0; xx--)
+                Console.Write("\t");
         }
     }
 }
