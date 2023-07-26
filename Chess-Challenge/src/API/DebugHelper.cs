b0ce437ce88f492c157aabda1286c60646b8b153
GioeleBucci <gioelebucci@gmail.com>
2023-07-26 16:51:09 +0200

    Add debugging utilities (including Stockfish)
    - New DebugHelper class
    	Added some utility functions, and a function that returns Stockfish's best move
    - New Stockfish class able to run Stockfish in the background

---
+++ Chess-Challenge/src/API/DebugHelper.cs
@@ -0,0 +1,60 @@
+﻿
+namespace ChessChallenge.API {
+    using ChessChallenge.Application.APIHelpers;
+    using ChessChallenge.Chess;
+    using System;
+        
+    /// <summary>
+    /// Custom class that offers tools for debugging purposes
+    /// </summary>
+    public class DebugHelper {
+        bool flag = true;
+        Stockfish engine;
+        /// <summary>
+        /// Outputs the color you're playing as
+        /// </summary>
+        public void PlayingAs(Board board) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            if (!flag) return;
+            flag = false;
+            string color = board.IsWhiteToMove ? "White" : "Black";
+            Console.WriteLine("Playing as " + color + "\nPress any key to continue");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Outputs the move and the respective evaluation according to Eval function.
+        /// </summary>
+        public void LogCurrentMove(Board board, Move move, Func<Board,int> Eval) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Move: " + move.StartSquare.Name + move.TargetSquare.Name);
+            board.MakeMove(move);
+            Console.WriteLine("Evaluation score: " + Eval(board) + "\nPress any key to continue");
+            board.UndoMove(move);
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Start Stockfish if not already running
+        /// </summary>
+        private void StockfishStart(string path) {
+            if(engine == null)
+                engine = new Stockfish(path);
+        }
+
+        /// <summary>
+        /// Returns the best move according to Stockfish.
+        /// If halt is true the program will wait for a key press.
+        /// </summary>
+        public void BestEngineMove(Board board, int searchDepth, bool halt, string stockfishPath) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            StockfishStart(stockfishPath);
+            string output = engine.BestMove(board.GetFenString(), searchDepth);
+            Console.WriteLine("Best engine move: " + output);
+            if (halt) Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Chess-Challenge/src/API/Stockfish.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+public class Stockfish : IDisposable {
+    private Process stockfishProcess;
+
+    public Stockfish(string path) {
+        stockfishProcess = new Process();
+        stockfishProcess.StartInfo.FileName = path;
+        stockfishProcess.StartInfo.UseShellExecute = false;
+        stockfishProcess.StartInfo.RedirectStandardInput = true;
+        stockfishProcess.StartInfo.RedirectStandardOutput = true;
+        stockfishProcess.StartInfo.CreateNoWindow = true;
+
+        stockfishProcess.Start();
+    }
+
+    /// <summary>
+    /// Returns the best move according to Stockfish. 
+    /// </summary>
+    public string BestMove(string fenPosition, int searchDepth) {
+        if (stockfishProcess == null || stockfishProcess.HasExited)
+            throw new InvalidOperationException("Stockfish process is not running (check the program path)");
+
+        stockfishProcess.StandardInput.WriteLine("uci");
+        stockfishProcess.StandardInput.WriteLine("position fen " + fenPosition);
+        stockfishProcess.StandardInput.WriteLine("go depth " + searchDepth);
+
+        string bestMove = string.Empty;
+        while (true) {
+            string line = stockfishProcess.StandardOutput.ReadLine();
+            if (line.StartsWith("bestmove")) {
+                bestMove = line.Split(' ')[1];
+                break;
+            }
+        }
+        return bestMove;
+    }
+
+    public void Dispose() {
+        if (stockfishProcess != null && !stockfishProcess.HasExited) {
+            stockfishProcess.StandardInput.WriteLine("quit");
+            stockfishProcess.WaitForExit(500);
+            stockfishProcess.Dispose();
+        }
+    }
+}
